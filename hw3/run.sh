7572bfe6ef3b13fe507b6c818d94358c3697127d
Jeff <jeff99955@csie.ntu.edu.tw>
2022-04-28 11:36:08 +0800

    fix run.sh

--- hw3/run.sh
+++ hw3/run.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=0
-python3 inference.py --num_beams 5 --batch_size 32
+python3 inference.py --num_beams 5 --batch_size 32 --test_file $1 --out_json $2
 unset CUDA_VISIBLE_DEVICES
